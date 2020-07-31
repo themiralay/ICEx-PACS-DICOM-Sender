745e19d410fc1424ccf728f213255bca1325a59a
Enes Can IŞIK <enescan.isik.378@gmail.com>
2020-07-31 21:47:02 +0300

    DICOM File Sender for PACS ImageServer

---
+++ pacstestt/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace pacstestt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Ayar = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.dosya = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.sinif = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.transfer = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.durum = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.Ayar.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(163, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "KONTROL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(56, 1);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "ENS";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(56, 30);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "192.168.1.146";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "AE Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "IP Adres";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Port";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "TimeOut";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(163, 33);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(87, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Security TLS";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(56, 56);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 105;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            105,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(56, 83);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 11;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            32768,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(56, 109);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown3.TabIndex = 12;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "maxPDU";
+            // 
+            // Ayar
+            // 
+            this.Ayar.Controls.Add(this.tabPage1);
+            this.Ayar.Controls.Add(this.tabPage2);
+            this.Ayar.Controls.Add(this.tabPage3);
+            this.Ayar.Location = new System.Drawing.Point(2, 0);
+            this.Ayar.Name = "Ayar";
+            this.Ayar.SelectedIndex = 0;
+            this.Ayar.Size = new System.Drawing.Size(259, 199);
+            this.Ayar.TabIndex = 106;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.numericUpDown3);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.numericUpDown2);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.numericUpDown1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.checkBox1);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(251, 173);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Control";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.listView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(251, 173);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Sender";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(167, 144);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "YOLLA";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(60, 144);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(46, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "SİL";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 144);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(48, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "EKLE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.dosya,
+            this.sinif,
+            this.transfer,
+            this.durum});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(251, 138);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // dosya
+            // 
+            this.dosya.Text = "dosya";
+            // 
+            // sinif
+            // 
+            this.sinif.Text = "sinif";
+            // 
+            // transfer
+            // 
+            this.transfer.Text = "transfer";
+            // 
+            // durum
+            // 
+            this.durum.Text = "durum";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(251, 173);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Receiver";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(111, 151);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(134, 13);
+            this.label6.TabIndex = 106;
+            this.label6.Text = "Develop By Enes Can IŞIK";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(258, 195);
+            this.Controls.Add(this.Ayar);
+            this.Name = "Form1";
+            this.Text = "ICEx PACS DICOM ";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.Ayar.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TabControl Ayar;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader dosya;
+        private System.Windows.Forms.ColumnHeader sinif;
+        private System.Windows.Forms.ColumnHeader transfer;
+        private System.Windows.Forms.ColumnHeader durum;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ pacstestt/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using Dicom.Network.Client;
+using Dicom.Network;
+using Dicom.Data;
+
+namespace pacstestt
+{
+    public delegate void BoolDelegate(bool state);
+    public delegate void MessageBoxDelegate(string message, string caption, bool isError);
+
+    public partial class Form1 : Form
+    {
+        private DicomTransferSyntax[] TransferSyntaxes;
+        private string ConfigPath = null;
+        private MyConfig Config = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            ThreadPool.QueueUserWorkItem(new WaitCallback(RunDicomEcho));
+        }
+
+        private void RunDicomEcho(object state)
+        {
+            bool success = false;
+            string msg = "Hata!";
+            try
+            {
+                CEchoClient scu = new CEchoClient();
+                scu.CallingAE =textBox1.Text;
+                scu.CalledAE = textBox1.Text;
+                scu.MaxPduSize = (uint)numericUpDown2.Value;
+                scu.ConnectTimeout = (int)numericUpDown3.Value;
+                scu.SocketTimeout = (int)numericUpDown3.Value;
+                scu.DimseTimeout = (int)numericUpDown3.Value;
+                scu.OnCEchoResponse += delegate (byte presentationId, ushort messageId, DcmStatus status) {
+                    msg = status.ToString();
+                };
+                scu.Connect(textBox2.Text, (int)numericUpDown1.Value, false ? DcmSocketType.TLS : DcmSocketType.TCP);
+                success = scu.Wait();
+
+                if (!success)
+                    msg = scu.ErrorMessage;
+            }
+            catch (Exception ex)
+            {
+                msg = ex.Message;
+            }
+
+            Invoke(new MessageBoxDelegate(ShowMessageBox), msg, "DICOM PACS Baglanti", !success);
+            Invoke(new BoolDelegate(ToggleEchoButtons), true);
+        }
+        public void ShowMessageBox(string message, string caption, bool isError)
+        {
+            MessageBox.Show(this, message, caption, MessageBoxButtons.OK, isError ? MessageBoxIcon.Error : MessageBoxIcon.Information);
+        }
+        private void ToggleEchoButtons(bool state)
+        {
+            button1.Enabled = state;
+        }
+        [Serializable]
+        public class MyConfig
+        {
+            public string LocalAE = "ENES";
+            public string RemoteAE = "ENES";
+            public string RemoteHost = "192.168.1.146";
+            public int RemotePort = 104;
+            public uint MaxPdu = 16384;
+            public int Timeout = 30;
+            public int TransferSyntax = 0;
+            public int Quality = 90;
+            public bool TLS = false;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Multiselect = true;
+            if (fd.ShowDialog(this) == DialogResult.OK)
+            {
+                foreach (string filename in fd.FileNames)
+                {
+                    try
+                    {
+                        CStoreRequestInfo info = new CStoreRequestInfo(filename);
+
+                        ListViewItem item = new ListViewItem(filename, 0);
+                        item.SubItems.Add(info.SOPClassUID.Description);
+                        item.SubItems.Add(info.TransferSyntax.UID.Description);
+                        item.SubItems.Add(info.Status.Description);
+
+                        item.Tag = info;
+                        info.UserState = item;
+
+                        listView1.Items.Add(item);
+                    }
+                    catch { }
+                }
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            Durum(false);
+
+
+            CStoreClient scu = new CStoreClient();
+            scu.DisableFileStreaming = true;
+            scu.CallingAE = textBox1.Text;
+            scu.CalledAE = textBox1.Text;
+            scu.MaxPduSize = (uint)numericUpDown2.Value;
+            scu.ConnectTimeout = (int)numericUpDown3.Value;
+            scu.SocketTimeout = (int)numericUpDown3.Value;
+            scu.DimseTimeout = (int)numericUpDown3.Value;
+            scu.SerializedPresentationContexts = true;
+            //scu.PreferredTransferSyntax = TransferSyntaxes[null];
+
+            //if (scu.PreferredTransferSyntax == DicomTransferSyntax.JPEGProcess1 ||
+            //    scu.PreferredTransferSyntax == DicomTransferSyntax.JPEGProcess2_4) {
+            //    DcmJpegParameters param = new DcmJpegParameters();
+            //    param.Quality = Config.Quality;
+            //    scu.PreferredTransferSyntaxParams = param;
+            //}
+            //else if (scu.PreferredTransferSyntax == DicomTransferSyntax.JPEG2000Lossy) {
+            //    DcmJpeg2000Parameters param = new DcmJpeg2000Parameters();
+            //    param.Rate = Config.Quality;
+            //    scu.PreferredTransferSyntaxParams = param;
+            //}
+
+            scu.OnCStoreResponseReceived = delegate (CStoreClient client, CStoreRequestInfo info) {
+                Invoke(new CStoreRequestCallback(GonderInfo), client, info);
+            };
+
+            foreach (ListViewItem lvi in listView1.Items)
+            {
+                lvi.ImageIndex = 0;
+                lvi.SubItems[3].Text = "Pending";
+
+                CStoreRequestInfo info = (CStoreRequestInfo)lvi.Tag;
+                scu.AddFile(info);
+            }
+
+            ThreadPool.QueueUserWorkItem(new WaitCallback(DicomSender), scu);
+        }
+        private void DicomSender(object state)
+        {
+            CStoreClient scu = (CStoreClient)state;
+            scu.Connect(textBox2.Text, (int)numericUpDown1.Value, checkBox1.Checked ? DcmSocketType.TLS : DcmSocketType.TCP);
+            if (!scu.Wait())
+                Invoke(new MessageBoxDelegate(ShowMessageBox), scu.ErrorMessage, "DICOM C-Store Error", true);
+
+            Invoke(new BoolDelegate(Durum), true);
+        }
+        private void Durum(bool state)
+        {
+            button4.Enabled = state;
+            button3.Enabled = state;
+            button2.Enabled = state;
+        }
+        private void GonderInfo(CStoreClient client, CStoreRequestInfo info)
+        {
+            ListViewItem lvi = (ListViewItem)info.UserState;
+            lvi.ImageIndex = (info.Status == DcmStatus.Success) ? 1 : 2;
+            lvi.SubItems[3].Text = info.Status.Description;
+            listView1.EnsureVisible(lvi.Index);
+        }
+    }
+}
